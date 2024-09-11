fba3ef65be8253fb0607bfae7f207b292702bcc9
esauoliveira <esauoliveiraz@gmail.com>
2024-09-10 21:02:47 -0300

    fundamentos 003

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value:'ler um livro',
+    checked: true
+}
+
+let metas =  [
+    meta,
+    {
+        value:"correr 5km todos os dias",
+        checked: false 
+    }
+]
+
+console.log(metas[1].value)
